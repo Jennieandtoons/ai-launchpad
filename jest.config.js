38c1d5817066177d4338a0f9dca866fb0258acc0
jennieandtoons <jennieandtoons@hotmail.com>
2025-01-09 10:46:02 +0100

    fix: add testing configuration and fix TypeScript setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest'
+  }
+};
